0c90df81255d6751fadcee98c295a7f0db311036
Ravi Sankar Rao <ravisankar139@gmail.com>
2016-08-19 00:52:08 +0530

    First git hub code. First console app in dot net core 1.0

---
+++ FirstdotNetCore/src/FirstdotNetCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstdotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from dot NET Core 1.0");
+        }
+    }
+}
